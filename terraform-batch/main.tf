b4a3ff47acc25adeb4f40e32d9cb5c444a5bd86d
Gajju9191 <gajananw9191@gmail.com>
2026-06-10 12:43:14 +0530

    rpochs incresed

--- terraform-batch/main.tf
+++ terraform-batch/main.tf
@@ -277,22 +277,3 @@ resource "aws_cloudwatch_event_target" "batch_job" {
 
   role_arn = aws_iam_role.eventbridge_role.arn
 }
-
-# ============================================
-# Outputs
-# ============================================
-output "ecr_repository_url" {
-  value = aws_ecr_repository.training.repository_url
-}
-
-output "batch_job_queue" {
-  value = aws_batch_job_queue.training.name
-}
-
-output "batch_job_definition" {
-  value = aws_batch_job_definition.retraining.name
-}
-
-output "batch_job_definition_revision" {
-  value = aws_batch_job_definition.retraining.revision
-}
